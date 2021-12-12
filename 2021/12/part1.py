a13187c413c13be17fd29a2a8e45678b96e7c925
Justin Lewis <jlew.blackout@gmail.com>
2021-12-12 09:01:21 -0500

    Day 12

---
+++ 2021/12/part1.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+with open('input.txt') as f:
+    edge_graph = defaultdict(list)
+    for line in f.readlines():
+        (edge_start, edge_end) = line.strip().split("-")
+        edge_graph[edge_start].append(edge_end)
+        edge_graph[edge_end].append(edge_start)
+
+# print(edge_graph.items())
+
+valid_paths = []
+def build_path(current_node, path_taken):
+    for node in edge_graph[current_node]:
+        current_path_taken = path_taken + [node]
+        if node == 'end':
+            valid_paths.append(current_path_taken)
+        else:
+            if node.isupper() or node not in path_taken:
+                build_path(node, current_path_taken)
+
+for node in edge_graph['start']:
+    current_path_taken = ['start'] + [node]
+    build_path(node,current_path_taken)
+
+for path in valid_paths:
+    print(','.join(path))
+
+print(len(valid_paths))
